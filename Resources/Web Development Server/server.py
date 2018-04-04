c912e511a4514d922bdf283c6f3c67c4e9ca285f
audragon <patrickliem@Patrick-Liems-MacBook-Air.local>
2018-04-04 19:38:05 -0400

    Changed picture of self, updated merch

--- Resources/Web Development Server/server.py
+++ Resources/Web Development Server/server.py
@@ -14,13 +14,8 @@ def hello_world():
 def about(name=None):
     if name is None:
         return render_template("about_me_template.html")
-<<<<<<< HEAD
     
-    return render_template("about_{0}.html".format(name))
-=======
-
     return render_template("about_{0}.html".format(name), user=name)
->>>>>>> c2c64da17af16930fb10b88c500896b79b2c5a29
 
 
 if __name__ == '__main__':
